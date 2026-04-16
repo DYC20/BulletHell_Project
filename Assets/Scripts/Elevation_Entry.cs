7df88af9cc13e25c677a9b040a2cb4b035a31d18
DYC20 <yd.carolla@gmail.com>
2026-04-16 17:17:51 +0300

    unsaved changes from before crash from the IDE shelf

--- Assets/Scripts/Elevation_Entry.cs
+++ Assets/Scripts/Elevation_Entry.cs
@@ -3,33 +3,15 @@ using UnityEngine.InputSystem;
 
 public class Elevation_Entry : MonoBehaviour
 {
-    [SerializeField] private Collider2D[] mountainColliders;
-    [SerializeField] private Collider2D[] boundryColliders;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        { 
-            PlayerController controller = other.GetComponentInParent<PlayerController>();
-            
-            if (controller != null)
-                controller.SetPlayerGrounded(false);
-            
-            foreach (Collider2D _mountain in mountainColliders)
-            {
-                _mountain.enabled = false;
-                
-            } 
-            foreach (Collider2D _Boundry in boundryColliders)
-            {
-                _Boundry.enabled = true;
-            }
-        }
-        foreach (SpriteRenderer sr in other.GetComponentsInChildren<SpriteRenderer>(true))
-        {
-            sr.sortingLayerName = "HighGround";
-            sr.sortingOrder = 2;
-        }
+        if (!other.CompareTag("Player"))
+            return;
 
+        PlayerElevationController elevation = other.GetComponentInParent<PlayerElevationController>();
+        if (elevation != null)
+        {
+            elevation.SetHighGround();
+        }
     }
 }
--- Assets/Scripts/Elevation_Exit.cs
+++ Assets/Scripts/Elevation_Exit.cs
@@ -5,45 +5,15 @@ using UnityEngine;
 
 public class Elevation_Exit : MonoBehaviour
 {
-    public Collider2D[] mountainColliders;
-    public Collider2D[] boundryColliders;
-    
-    IEnumerator CheckNextFrame(Collider2D c)
-    {
-        WaitForSeconds wait = new WaitForSeconds(1f);
-        yield return wait; // wait 1 frame
-        Debug.Log($"NEXT FRAME -> {c.name} enabled:{c.enabled}", c.gameObject);
-    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        { 
-            PlayerController controller = other.GetComponentInParent<PlayerController>();
-            
-            if (controller != null)
-                controller.SetPlayerGrounded(true);
-            
-            foreach (Collider2D _mountain in mountainColliders)
-            {
-                _mountain.enabled = true;
-                Debug.Log(
-                    $"ENABLED -> {_mountain.name} | enabled:{_mountain.enabled} | activeInHierarchy:{_mountain.gameObject.activeInHierarchy} | instanceID:{_mountain.GetInstanceID()}",
-                    _mountain.gameObject);
-             
-                StartCoroutine(CheckNextFrame(_mountain));
-                
-            } 
-            foreach (Collider2D _Boundry in boundryColliders)
-            {
-                _Boundry.enabled = false;
-            }
-        
-            foreach (SpriteRenderer sr in other.GetComponentsInChildren<SpriteRenderer>(true))
-            {
-              sr.sortingLayerName = "Ground";
-             sr.sortingOrder = 1;
-            }
-        //other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        if (!other.CompareTag("Player"))
+            return;
+
+        PlayerElevationController elevation = other.GetComponentInParent<PlayerElevationController>();
+        if (elevation != null)
+        {
+            elevation.SetLowGround();
         }
     }
 }
